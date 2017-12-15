9a3e4f831bb83c353955f5b6707117ad4072e7c9
husky-parul <singh.p@husky.neu.edu>
2017-12-15 16:05:14 -0500

    BuildControls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,19 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
-    const tranformedBurgerIngredient=Object.keys(props.ingredients)
+    let tranformedBurgerIngredient=Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_,i) => {
                 return <BurgerIngredient key={igKey+i} type={igKey}/>
             });
-        });
+        }).reduce((arr, el) => {
+            return arr.concat(el)
+
+        }, []);
+        console.log(tranformedBurgerIngredient);
+        if(tranformedBurgerIngredient.length === 0){
+            tranformedBurgerIngredient=<p>Please add some ingredients</p>;
+        }
 
     return(
         <div className={classes.Burger}>
