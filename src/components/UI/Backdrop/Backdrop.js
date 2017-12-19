8e53dee5788acb326aade6b5757f771ac116349f
husky-parul <singh.p@husky.neu.edu>
2017-12-19 15:32:38 -0500

    trying with backdrop

---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Backdrop'
+
+const backdrop = ( props ) => (
+     props.show ? <div 
+        className={classes.Backdrop}>
+        </div> : null
+);
+
+export default backdrop;
