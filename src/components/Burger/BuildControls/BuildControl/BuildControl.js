a38c857c6ec036e9f4b8b8736678a94698e551ae
husky-parul <singh.p@husky.neu.edu>
2017-12-20 17:20:03 -0500

    basic mobile version done

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,13 +1,16 @@
 import React from 'react';
+
 import classes from './BuildControl.css';
 
-const buildControl = ( props ) => (
+const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Label}
-            onClick={props.removed}
+        <button 
+            className={classes.Less} 
+            onClick={props.removed} 
             disabled={props.disabled}>Less</button>
-        <button className={classes.More}
+        <button 
+            className={classes.More} 
             onClick={props.added}>More</button>
     </div>
 );
--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,30 @@
 import React from 'react';
+
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
 const orderSummary = ( props ) => {
-    const ingredientsSummary=Object.keys(props.ingredients)
-        .map(igKey =>{
-            return(
+    const ingredientSummary = Object.keys( props.ingredients )
+        .map( igKey => {
+            return (
                 <li key={igKey}>
-                    <span style={{testTransfor: 'capitalize'}}> {igKey}</span>: {props.ingredients[igKey]}
-                </li>);
-        });
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+                </li> );
+        } );
+
     return (
         <Aux>
-            <h3>Your Orders</h3>
-            <p>A delicious burder with following add-ons:</p>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
             <ul>
-                {ingredientsSummary}
+                {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price}</strong></p>
-            <p>Continue to checkout</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceld}>CANCEL</Button>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p>Continue to Checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
             <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
         </Aux>
     );
-
 };
 
 export default orderSummary;
